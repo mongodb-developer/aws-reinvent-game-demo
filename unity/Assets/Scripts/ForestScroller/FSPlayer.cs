cdf3e954529a8d410655cda5020fea4a5d080c2c
Nic Raboy <nic@nraboy.com>
2021-09-23 07:22:11 -0700

    Change Midgard Music. Start Forest Side Scroller

---
+++ unity/Assets/Scripts/ForestScroller/FSPlayer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSPlayer : MonoBehaviour {
+
+    private Rigidbody2D _rb2d;
+    private bool _isGrounded = true;
+
+    [Range(1, 10)]
+    public float movementSpeed = 5.0f;
+
+    [Range(1, 10)]
+    public float jumpVelocity = 7.0f;
+
+    [Range(1, 5)]
+    public float fallingMultiplier = 3.0f;
+
+    void Start() {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+        } else if(Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.Space) && _isGrounded == true) {
+            _rb2d.velocity += Vector2.up * jumpVelocity;
+            _isGrounded = false;
+        }
+
+        if((_rb2d.velocity.y < 0) || (_rb2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space))) {
+            _rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallingMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.collider.name == "Ground") {
+            _isGrounded = true;
+        }
+    }
+
+}
