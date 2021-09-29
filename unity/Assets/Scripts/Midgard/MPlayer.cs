7671e84ecb35445035b95fbfaf6a03ccdefdff84
Nic Raboy <nic@nraboy.com>
2021-09-29 10:35:08 -0700

    Add Cat to Midgard. Update Animations for Cat

--- unity/Assets/Scripts/Midgard/MPlayer.cs
+++ unity/Assets/Scripts/Midgard/MPlayer.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MPlayer : MonoBehaviour {
 
+    private Animator _animator;
+
     public float movementSpeed = 5.0f;
 
     void Awake() {
@@ -13,23 +15,43 @@ public class MPlayer : MonoBehaviour {
         }
     }
 
-    void Start() { }
+    void Start() {
+        _animator = GetComponent<Animator>();
+    }
 
     void FixedUpdate() {
         if(Input.GetKey(KeyCode.UpArrow)) {
+            ResetAnimations();
+            _animator.SetBool("doWalkUp", true);
             transform.position += Vector3.up * movementSpeed * Time.deltaTime;
         } else if(Input.GetKey(KeyCode.DownArrow)) {
+            ResetAnimations();
+            _animator.SetBool("doWalkDown", true);
             transform.position += Vector3.down * movementSpeed * Time.deltaTime;
         } else if(Input.GetKey(KeyCode.LeftArrow)) {
+            ResetAnimations();
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            _animator.SetBool("doWalk", true);
             transform.position += Vector3.left * movementSpeed * Time.deltaTime;
         } else if(Input.GetKey(KeyCode.RightArrow)) {
+            ResetAnimations();
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+            _animator.SetBool("doWalk", true);
             transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        } else {
+            ResetAnimations();
         }
         if(Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)) {
             if(RealmController.Instance != null) {
                 RealmController.Instance.UpdatePositionInMidgard(transform.position.x, transform.position.y);
             }
         }
+    }
+
+    private void ResetAnimations() {
+        _animator.SetBool("doWalk", false);
+        _animator.SetBool("doWalkDown", false);
+        _animator.SetBool("doWalkUp", false);
     }
 
 }
