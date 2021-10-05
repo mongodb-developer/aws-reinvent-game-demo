88c531ce31862ed828694ac3842a3222de529a04
Nic Raboy <nic@nraboy.com>
2021-10-05 13:47:48 -0700

    Multi-Segment NPC Story Messaging
    NPC characters can now show a minimum of 1 message rather than a maximum of only 1 message. This is done through a 'List' on the NPC.

--- unity/Assets/Scripts/Midgard/MStoryModal.cs
+++ unity/Assets/Scripts/Midgard/MStoryModal.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 
 public class MStoryModal : MonoBehaviour {
 
-    public string storyMessage;
     public Sprite characterIcon;
+    public List<string> storyMessages;
 
     private Text _storyText;
     private SpriteRenderer _spriteRenderer;
+    private int _messageNumber = 0;
 
     void OnEnable() {
         Time.timeScale = 0.0f;
@@ -17,14 +18,20 @@ public class MStoryModal : MonoBehaviour {
         GameObject characterImageGameObject = transform.Find("NPCImage").gameObject;
         _storyText = storyTextGameObject.GetComponent<Text>();
         _spriteRenderer = characterImageGameObject.GetComponent<SpriteRenderer>();
-        _storyText.text = storyMessage;
+        _storyText.text = storyMessages[_messageNumber];
         _spriteRenderer.sprite = characterIcon;
     }
 
     void Update() {
-        if(Input.GetKey(KeyCode.Return)) {
-            Time.timeScale = 1.0f;
-            gameObject.SetActive(false);
+        if(Input.GetKeyUp(KeyCode.Return)) {
+            if(_messageNumber < storyMessages.Count - 1) {
+                _messageNumber++;
+                _storyText.text = storyMessages[_messageNumber];
+            } else {
+                _messageNumber = 0;
+                Time.timeScale = 1.0f;
+                gameObject.SetActive(false);
+            }
         }
     }
 
